48aa161a536c04e78d16e42b0c316cf50652fd3d
EVAOTA <nervevawaka@ezweb.ne.jp>
2018-02-14 15:39:23 +0900

    first formal commir

---
+++ compile.sh
@@ -0,0 +1,37 @@
+#
+# File-Name: Makefile
+#
+# Created in: 2018/2/13
+# Author: Ryo Wakabayashi
+#
+
+PLATEX="uplatex"
+RM="rm -f -r"
+DVI2PDF="dvipdfmx" 
+
+INFODIR="./Info"
+DOCDIR="./Doc"
+ 
+CHARCODE="nkf -g *.tex"  
+case $CHARCODE in # nkf が返す文字コードにあわせる
+    UTF-8) KANJI="-kanji=utf8";;
+    EUC-JP) KANJI="-kanji=euc";;
+    Shift-JIS) KANJI="kanji=sjis";;
+    ISO-2022-JP) KANJI="-kanji=jis";;
+esac
+
+mkdir -p $DOCDIR
+mkdir -p $INFODIR
+
+cd $INFODIR
+${PLATEX} $KANJI ../*.tex
+cd ../
+
+${DVI2PDF} -o $DOCDIR/output.pdf $INFODIR/*.dvi 
+
+evince $DOCDIR/*.pdf &
+
+
+# $(RM) $(INFODIR) $(DOCDIR)
+
+
--- install.sh
+++ install.sh
@@ -1,23 +1,19 @@
 #! /bin/sh
 
-echo "installing git..." 
-sudo apt-get install git
-
-echo "installing texlive..."
 sudo apt-get -y install texlive 											# 本体 
 
-echo "installing texlive-lang-cjk..."
 sudo apt-get -y install texlive-lang-cjk # 日本語版 (UTF8 対応) 
 
-echo "installing txdvik-ja..."
+sudo apt-get -y install texlive-fonts-recommended texlive-fonts-extra 
+
+
 sudo apt-get -y install xdvik-ja 										# プレビューを見るのに必要 
 
-echo "installing dvipsk-ja..."
 sudo apt-get -y install dvipsk-ja        # DVI から PostScript への変換用 
 
-echo "installing gv..."
 sudo apt-get -y install gv               # Ghostscript 形式の図を取り込んだ文書の作成用 
 
-echo "installing fonts for texlive..."
-sudo apt-get -y install texlive-fonts-recommended texlive-fonts-extra 
+sudo chmod +x compile.sh
+sudo chmod +x uninstall.sh
+
 
